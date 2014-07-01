c8179113255727baeaa436261678ea7756a21797
Tian Song <tian.y.song@gmail.com>
2014-07-01 10:19:31 -0700

    installed cookie parser.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 app.get('/', function(req, res){
   //some server code related to cookies
+  app.use(express.cookieParser('sercret cookies'));
+  req.cookies.name;
 
   res.sendfile(path.join(__dirname, 'public/index.html'));
 });
