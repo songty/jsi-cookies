285f4fcf706bfea2c6ab83d631e6fa9160cf7dd3
Tian Song <tian.y.song@gmail.com>
2014-07-01 11:08:28 -0700

    takes the difference of cookie access

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ app.use(cookieParser());
 
 app.get('/', function(req, res){
   //some server code related to cookies
-   console.log(req.cookies());
-  res.sendfile(path.join(__dirname, 'public/index.html'));
+   console.log(req.cookies);    
+   console.log('last access by user:' + (Date.now() - 
+   	req.cookies['last-request-time'])/1000 +'s ago');
+   res.cookie('last-request-time', Date.now());
+   res.sendfile(path.join(__dirname, 'index.html'));
 });
 app.listen(9000);
