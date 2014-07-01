b2809b21486351a9c4ee42e7c536b181ab0d99d5
Tian Song <tian.y.song@gmail.com>
2014-07-01 10:32:45 -0700

    console log cookie.

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var path = require('path');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
+
+app.use(cookieParser());
+
 app.get('/', function(req, res){
   //some server code related to cookies
-  app.use(express.cookieParser('sercret cookies'));
-  req.cookies.name;
-
+   console.log(req.cookies());
   res.sendfile(path.join(__dirname, 'public/index.html'));
 });
 app.listen(9000);
