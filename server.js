a0f0d998724ed03c8db1a1ddea961b1641eb5057
Tian Song <tian.y.song@gmail.com>
2014-08-03 20:10:37 -0700

    setting up a new mac.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
 
-app.use(cookieParser());
+  app.use(cookieParser());
 
 app.get('/', function(req, res){
   //some server code related to cookies
